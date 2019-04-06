af39581bb43d804ff5fa7b0a48495008ace2db76
Hoang Lam <vhlam1997@live.com>
2019-04-06 14:35:39 +0700

    init config circleci

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@ import {processData} from "./services";
 import fs from "fs";
 import path from "path";
 import {createXlSXfile} from "./utils/xlsx";
-// import {}
+
 if (process.env.NODE_ENV === 'development') {
     require('dotenv').config()
 }
 
-(async () => {
+(async() => {
     const dir = path.join(__dirname, `/exportData/`)
 
     if (!fs.existsSync(dir)) {
         await fs.mkdirSync(dir, {recursive: false})
     }
+
      const data = await processData();
     createXlSXfile({data, dirName: 'src/exportData'})
 })();
+
 
 
 
